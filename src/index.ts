3c30438dd2a555f93f613788d24802638f05df98
lukasabbe <67807954+lukasabbe@users.noreply.github.com>
2025-01-13 21:38:11 +0100

    fixed problem

--- src/index.ts
+++ src/index.ts
@@ -2,3 +2,4 @@ export { GamerBotAPI } from "./classes/gamerbot.js";
 export { PorfileData } from "./classes/ProfileData.js";
 export { GuildData } from "./classes/GuildData.js";
 export { ConfigData } from "./classes/ConfigData.js";
+export { Models } from "./classes/models.js";
